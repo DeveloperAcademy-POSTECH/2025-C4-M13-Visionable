7375b79d438066fb0337d8b0148224f52210cb65
Jamin <jamin@Jaminui-MacBookAir.local>
2025-07-11 14:45:15 +0900

    [Feat] #6 - locallizable 설정

---
+++ FFIP-iOS/FFIP-iOS/Sources/Components/FfipTextField.swift
@@ -0,0 +1,18 @@
+//
+//  FfipTextField.swift
+//  FFIP-iOS
+//
+//  Created by Jamin on 7/11/25.
+//
+
+import SwiftUI
+
+struct FfipTextField: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FfipTextField()
+}
---
+++ FFIP-iOS/FFIP-iOS/Sources/Components/RecentSearchRow.swift
@@ -0,0 +1,18 @@
+//
+//  RecentSearchRow.swift
+//  FFIP-iOS
+//
+//  Created by Jamin on 7/11/25.
+//
+
+import SwiftUI
+
+struct RecentSearchRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecentSearchRow()
+}
