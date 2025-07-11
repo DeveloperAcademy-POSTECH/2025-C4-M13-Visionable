364aca449973ae61433c8c8dc8083c8ff872982c
Jamin <jamin@Jaminui-MacBookAir.local>
2025-07-11 14:46:56 +0900

    [Feat] #6 - SearchModel 작성

--- FFIP-iOS/FFIP-iOS/Sources/Presentation/Search/SearchModel.swift
+++ FFIP-iOS/FFIP-iOS/Sources/Presentation/Search/SearchModel.swift
@@ -8,4 +8,19 @@
 import SwiftUI
 
 @Observable
-final class SearchModel {}
+final class SearchModel {
+    var searchKeyword: String = ""
+    var recentSearchKeyword: [String] = ["최근1", "최근2"]
+    
+    func submitSearch() {
+        // TODO: 검색 키워드 전달하는 로직
+    }
+    
+    func fetchRecentSearchKeywords() {
+        // TODO: UserDefaults로 최근 검색어 목록 가져오는 로직
+    }
+    
+    func deleteRecentSearchKeyword(_ keyword: String) {
+        // TODO: 해당 keyword를 recentSearchKeyword 배열에서 제거
+    }
+}
