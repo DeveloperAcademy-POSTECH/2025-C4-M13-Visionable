1bc131f1e4c2cd690fae626ecb9e646da51d1cdd
Jamin <jamin@Jaminui-MacBookAir.local>
2025-07-11 14:46:02 +0900

    [Feat] #6 - FfipTextField 컴포넌트 구현

--- FFIP-iOS/FFIP-iOS/Sources/Components/FfipTextField.swift
+++ FFIP-iOS/FFIP-iOS/Sources/Components/FfipTextField.swift
@@ -8,11 +8,40 @@
 import SwiftUI
 
 struct FfipTextField: View {
+    var placeholder: String
+    @Binding var text: String
+    
+    var onVoiceSearch: () -> Void
+    var onSubmit: () -> Void = {}
+    
+    private var isTextEmpty: Bool {
+        text.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            TextField(placeholder, text: $text)
+                .padding(.vertical, 16)
+                .submitLabel(.search)
+                .onSubmit(onSubmit)
+            
+            Button {
+                if isTextEmpty {
+                    onVoiceSearch()
+                } else {
+                    text = ""
+                }
+            } label: {
+                Image(systemName: isTextEmpty ? "mic.fill" : "xmark.circle.fill")
+            }
+            .tint(.black)
+        }
+        .padding(.horizontal, 20)
+        .background(.gray.opacity(0.2))
     }
 }
 
 #Preview {
-    FfipTextField()
+    @Previewable @State var text = "검색어"
+    FfipTextField(placeholder: "placeholder", text: $text, onVoiceSearch: {})
 }
