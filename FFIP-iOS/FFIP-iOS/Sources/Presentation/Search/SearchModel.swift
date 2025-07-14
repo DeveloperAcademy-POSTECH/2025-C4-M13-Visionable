b6bcd6883c733abf21d43c3b8c899b6277f50785
Jamin <jamin@10-141-41-107.0101.pos.kr.iosda.org>
2025-07-14 19:37:38 +0900

    [Feat] #18 - SearchModel 수정

--- FFIP-iOS/FFIP-iOS/Sources/Presentation/Search/SearchModel.swift
+++ FFIP-iOS/FFIP-iOS/Sources/Presentation/Search/SearchModel.swift
@@ -9,26 +9,22 @@ import SwiftUI
 
 @Observable
 final class SearchModel {
-    private let recentSearchKey = "recentSearchKeywords"
-    var searchKeyword: String = ""
-    var recentSearchKeywords: [String] = []
+    private(set) var recentSearchKeywords: [String]
     
-    func submitSearch() {
-        var recent = UserDefaults.standard.stringArray(forKey: recentSearchKey) ?? []
-        recent.removeAll(where: { $0 == searchKeyword })
-        recent.insert(searchKeyword, at: 0)
-        
-        recent = Array(recent.prefix(5))
-        
-        UserDefaults.standard.set(recent, forKey: recentSearchKey)
-    }
+    init(recentSearchKeywords: [String]) {
+            self.recentSearchKeywords = recentSearchKeywords
+        }
     
-    func fetchRecentSearchKeywords() {
-        recentSearchKeywords = UserDefaults.standard.stringArray(forKey: recentSearchKey) ?? []
+    func addRecentSearchKeyword(_ keyword: String) {
+        recentSearchKeywords.removeAll(where: { $0 == keyword })
+        recentSearchKeywords.insert(keyword, at: 0)
+        recentSearchKeywords = Array(recentSearchKeywords.prefix(5))
+        
+        UserDefaults.standard.set(recentSearchKeywords, forKey: UserDefaultsKey.recentSearch)
     }
     
     func deleteRecentSearchKeyword(_ keyword: String) {
         recentSearchKeywords.removeAll(where: { $0 == keyword })
-            UserDefaults.standard.set(recentSearchKeywords, forKey: recentSearchKey)
+        UserDefaults.standard.set(recentSearchKeywords, forKey: UserDefaultsKey.recentSearch)
     }
 }
