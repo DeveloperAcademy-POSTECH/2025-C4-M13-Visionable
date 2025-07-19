434518a451c57bebed26860fcdaa74b622c586d2
chxhyxn <chxhyxn@gmail.com>
2025-07-19 19:02:14 +0900

    [Feat] #30 - CameraLockIcon hifi 적용

--- FFIP-iOS/FFIP-iOS/Sources/Components/FfipCameraLockIcon.swift
+++ FFIP-iOS/FFIP-iOS/Sources/Components/FfipCameraLockIcon.swift
@@ -12,14 +12,8 @@ struct FfipCameraLockIcon: View {
     let show: Bool
 
     var body: some View {
-        Image(systemName: isPaused ? "lock.fill" : "lock.open.fill")
-            .foregroundColor(.white)
-            .font(.system(size: 16, weight: .bold))
-            .padding(16)
-            .background(
-                Circle()
-                    .fill(.black.opacity(0.4))
-            )
+        Image(isPaused ? "img_lock" : "img_lock_open")
+            .frame(width: 60, height: 60)
             .opacity(show ? 1 : 0)
     }
 }
