684222ae28a19b223ca4c7af626291afb7785341
Jamin <jamin@Jaminui-MacBookAir.local>
2025-07-20 17:19:37 +0900

    [Refactor] #31 - Search 하위 뷰들 SubView로 분리

---
+++ FFIP-iOS/FFIP-iOS/Sources/Presentation/Search/SubViews/RecentSearchFlow.swift
@@ -0,0 +1,30 @@
+//
+//  RecentSearchFlow.swift
+//  FFIP-iOS
+//
+//  Created by Jamin on 7/20/25.
+//
+
+import SwiftUI
+
+struct RecentSearchFlow: View {
+    let keywords: [String]
+    let onTap: (String) -> Void
+    let onTapDelete: (String) -> Void
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 16) {
+            ForEach(keywords, id: \.self) { keyword in
+                FfipRecentSearchCapsule(
+                    keyword: keyword,
+                    onTap: { onTap(keyword) },
+                    onTapDelete: { onTapDelete(keyword) }
+                )
+            }
+        }
+    }
+}
+
+#Preview {
+    RecentSearchFlow(keywords: ["1", "2"], onTap: { _ in }, onTapDelete: { _ in })
+}
--- FFIP-iOS/FFIP-iOS/Sources/Presentation/Search/SubViews/SearchTypeSelectionView.swift
+++ FFIP-iOS/FFIP-iOS/Sources/Presentation/Search/SubViews/SearchTypeSelectionView.swift
@@ -7,7 +7,28 @@
 
 import SwiftUI
 
-struct SearchTypeRow: View {
+struct SearchTypeSelectionView: View {
+    @Binding var selectedType: SearchType
+    
+    var body: some View {
+        VStack(spacing: 32) {
+            VStack(spacing: 24) {
+                ForEach(SearchType.allCases) { type in
+                    SearchTypeRow(
+                        title: String(localized: type.title),
+                        tagImage: type.tagIcon,
+                        isSelected: selectedType == type,
+                        onTap: {
+                            selectedType = type
+                        }
+                    )
+                }
+            }
+        }
+    }
+}
+
+private struct SearchTypeRow: View {
     private let title: String
     private let tagImage: ImageResource?
     private let isSelected: Bool
@@ -41,27 +62,6 @@ struct SearchTypeRow: View {
         .contentShape(Rectangle())
         .onTapGesture {
             onTap()
-        }
-    }
-}
-
-struct SearchTypeSelectionView: View {
-    @Binding var selectedType: SearchType
-    
-    var body: some View {
-        VStack(spacing: 32) {
-            VStack(spacing: 24) {
-                ForEach(SearchType.allCases) { type in
-                    SearchTypeRow(
-                        title: String(localized: type.title),
-                        tagImage: type.tagIcon,
-                        isSelected: selectedType == type,
-                        onTap: {
-                            selectedType = type
-                        }
-                    )
-                }
-            }
         }
     }
 }
