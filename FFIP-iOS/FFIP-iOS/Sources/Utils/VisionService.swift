b9940417a420ed900d16f16e9d61cbbf78d21367
chxhyxn <chxhyxn@gmail.com>
2025-07-23 09:28:00 +0900

    [Fix] #51 - Aesthetic score 삭제

--- FFIP-iOS/FFIP-iOS/Sources/Utils/VisionService.swift
+++ FFIP-iOS/FFIP-iOS/Sources/Utils/VisionService.swift
@@ -9,7 +9,6 @@ import Vision
 
 actor VisionService {
     private var recognizeTextRequest = RecognizeTextRequest()
-    private var aestheticRequest = CalculateImageAestheticsScoresRequest()
 
     func prepareTextRecognition(searchKeyword: String) {
         recognizeTextRequest.minimumTextHeightFraction = 0.01
@@ -25,13 +24,5 @@ actor VisionService {
         image: CVImageBuffer
     ) async throws -> [RecognizedTextObservation] {
         return try await recognizeTextRequest.perform(on: image)
-    }
-
-    func calculateAestheticScore(from buffer: CVImageBuffer) async throws
-        -> Float {
-        let observation = try await aestheticRequest.perform(on: buffer)
-        let score = observation.overallScore
-
-        return score
     }
 }
