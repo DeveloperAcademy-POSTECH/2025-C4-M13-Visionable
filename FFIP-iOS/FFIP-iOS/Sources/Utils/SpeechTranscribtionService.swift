0b9130619612bff08df88124d344242ca37f5d6d
Jamin <jamin@10-141-41-63.0101.pos.kr.iosda.org>
2025-07-24 12:05:42 +0900

    [Feat] #44 - SpeechTranscriptionService 구현..

---
+++ FFIP-iOS/FFIP-iOS/Sources/Utils/SpeechTranscribtionService.swift
@@ -0,0 +1,353 @@
+//
+//  SpeechTranscribtionService.swift
+//  FFIP-iOS
+//
+//  Created by Jamin on 7/23/25.
+//
+
+@preconcurrency import AVFoundation
+import Foundation
+import Speech
+
+actor SpeechTranscriptionService {
+    private enum TranscriptionError: Error {
+        case couldNotDownloadModel
+        case failedToSetupRecognitionStream
+        case invalidAudioDataType
+        case localeNotSupported
+        case noInternetForModelDownload
+        case audioFilePathNotFound
+
+        var descriptionString: String {
+            switch self {
+            case .couldNotDownloadModel:
+                return "Could not download the model."
+            case .failedToSetupRecognitionStream:
+                return "Could not set up the speech recognition stream."
+            case .invalidAudioDataType:
+                return "Unsupported audio format."
+            case .localeNotSupported:
+                return "This locale is not yet supported by SpeechAnalyzer."
+            case .noInternetForModelDownload:
+                return
+                    "The model could not be downloaded because the user is not connected to internet."
+            case .audioFilePathNotFound:
+                return "Couldn't write audio to file."
+            }
+        }
+    }
+
+    private var audioEngine: AVAudioEngine?
+    private var analyzerFormat: AVAudioFormat?
+
+    private var speechTranscriber: SpeechTranscriber?
+    private var speechDetector: SpeechDetector?
+    private var speechAnalyzer: SpeechAnalyzer?
+    private var speechStream: AsyncStream<AnalyzerInput>?
+    private var speechStreamContinuation:
+        AsyncStream<AnalyzerInput>.Continuation?
+    private var recognitionTask: Task<Void, Never>?
+
+    private let converter = BufferConverter()
+    private(set) var transcript: AttributedString = ""
+    var downloadProgress: Progress?
+
+    func startTranscribing() async throws {
+        try await prepareSpeechModules()
+        try prepareAudioEngine()
+        
+        Task {
+            try await testSpeechTranscriber()
+        }
+        
+        Task {
+            try await testSpeechDetector()
+        }
+    }
+
+    func testSpeechDetector() async throws {
+        guard let speechDetector else {
+            return
+        }
+        for try await case let result in speechDetector.results {
+            print("디텍터 \(result), \(Date.now)")
+        }
+    }
+    
+    func testSpeechTranscriber() async throws {
+        guard let speechTranscriber else {
+            return
+        }
+        
+        for try await case let result in speechTranscriber.results {
+            let text = result.text
+            print("트랜스크라이버 \(text), \(Date.now)")
+        }
+    }
+
+    func stopTranscribing() async {
+        //        recognitionTask?.cancel()
+        //        recognitionTask = nil
+        //        audioEngine?.stop()
+        //        inputBuilder?.finish()
+        //        try? await speechAnalyzer?.finalizeAndFinishThroughEndOfInput()
+    }
+
+    private func prepareSpeechModules() async throws {
+        let selectedLocale = Locale(languageCode: "ko_KR")
+
+        let transcriber = SpeechTranscriber(
+            locale: selectedLocale,
+            transcriptionOptions: [],
+            reportingOptions: [.volatileResults],
+            attributeOptions: []
+        )
+
+        print("transriber: \(transcriber)")
+        
+        let detector = SpeechDetector(
+            detectionOptions: .init(sensitivityLevel: .high),
+            reportResults: true
+        )
+        
+        self.speechTranscriber = transcriber
+        self.speechDetector = detector
+
+//        let modules = [detector, transcriber]
+//        speechAnalyzer = SpeechAnalyzer(modules: modules)
+//        
+//        guard let speechAnalyzer else { return }
+//
+//        self.analyzerFormat = await SpeechAnalyzer.bestAvailableAudioFormat(
+//            compatibleWith: modules)
+
+        (speechStream, speechStreamContinuation) = AsyncStream<AnalyzerInput>
+            .makeStream()
+
+        guard let speechStream else { return }
+
+        print("speechStream: \(speechStream)")
+
+        do {
+            try await ensureModel(
+                transcriber: transcriber,
+                locale: selectedLocale
+            )
+        } catch let error as TranscriptionError {
+            print(error)
+            return
+        }
+
+        //        recognitionTask = Task {
+        //            do {
+        //                for try await case let result in transcriber.results {
+        //                    let text = result.text
+        //                    print("text \(text)")
+        //                }
+        //            } catch {
+        //                print("speech recognition failed")
+        //            }
+        //        }
+
+        try await speechAnalyzer?.start(inputSequence: speechStream)
+        
+        // 에 잘넣기
+        // Mediator에서 잘 뽑기
+    }
+
+    private func prepareAudioEngine() throws {
+        print("preapreAudioEngine 시작")
+        let audioEngine = AVAudioEngine()
+
+        guard let analyzerFormat else {
+            throw TranscriptionError.failedToSetupRecognitionStream
+        }
+
+        let audioSession = AVAudioSession.sharedInstance()
+        try audioSession.setCategory(
+            .playAndRecord,
+            mode: .measurement,
+            options: .duckOthers
+        )
+        try audioSession.setActive(true, options: .notifyOthersOnDeactivation)
+
+        let inputNode = audioEngine.inputNode
+        let recordingFormat = inputNode.outputFormat(forBus: 0)
+
+        inputNode.installTap(
+            onBus: 0,
+            bufferSize: 1024,
+            format: recordingFormat
+        ) { [weak self] buffer, _ in
+            guard let self else { return }
+            Task {
+                try? await self.sendBufferToAnalyzer(buffer)
+            }
+        }
+
+        audioEngine.prepare()
+        try audioEngine.start()
+        self.audioEngine = audioEngine
+    }
+
+    //    private func startRecognitionTask() {
+    //        recognitionTask = Task {
+    //            await processRecognition()
+    //        }
+    //    }
+
+    private func sendBufferToAnalyzer(_ buffer: AVAudioPCMBuffer) async throws {
+        guard let speechStreamContinuation, let analyzerFormat else {
+            throw TranscriptionError.invalidAudioDataType
+        }
+
+        let converted = try await converter.convertBuffer(
+            buffer,
+            to: analyzerFormat
+        )
+        let input = AnalyzerInput(buffer: converted)
+        speechStreamContinuation.yield(input)
+    }
+    //
+    //    private func processRecognition() async {
+    //        guard let transcriber = speechTranscriber, let detector = speechDetector else { return }
+    //
+    //        let transcriberStream = Task {
+    //            for try await result in transcriber.results {
+    //                let text = result.text
+    //                await self.handleTranscriptUpdate(result: result, text: text)
+    //            }
+    //        }
+    //
+    //        let detectorStream = Task {
+    //            for try await result in detector.results {
+    //                await self.updateSpeechDetected(isDetected: result.speechDetected)
+    //            }
+    //        }
+    //
+    //        _ = try? await (transcriberStream.value, detectorStream.value)
+    //        }
+    //
+    //    private func handleTranscriptUpdate(result: SpeechTranscriber.Result, text: AttributedString) async {
+    //        print("handleTranscriptUpdate")
+    //        if result.isFinal {
+    //            self.transcript += text
+    //            self.transcript += " "
+    //        } else {
+    //            self.transcript = text
+    //        }
+    ////        transcriptStreamContinuation?.yield(self.transcript.description)
+    //    }
+
+    private func updateSpeechDetected(isDetected: Bool) async {
+        print("updateSpeechDetected")
+        //        speechDetectedStreamContinuation?.yield(isDetected)
+    }
+}
+
+extension SpeechTranscriptionService {
+    public func ensureModel(transcriber: SpeechTranscriber, locale: Locale)
+        async throws
+    {
+        guard await supported(locale: locale) else {
+            throw TranscriptionError.localeNotSupported
+        }
+
+        if await installed(locale: locale) {
+            return
+        } else {
+            try await downloadIfNeeded(for: transcriber)
+        }
+    }
+
+    func supported(locale: Locale) async -> Bool {
+        let supported = await SpeechTranscriber.supportedLocales
+        return supported.map { $0.identifier(.bcp47) }.contains(
+            locale.identifier(.bcp47)
+        )
+    }
+
+    func installed(locale: Locale) async -> Bool {
+        let installed = await Set(SpeechTranscriber.installedLocales)
+        return installed.map { $0.identifier(.bcp47) }.contains(
+            locale.identifier(.bcp47)
+        )
+    }
+
+    func downloadIfNeeded(for module: SpeechTranscriber) async throws {
+        if let downloader = try await AssetInventory.assetInstallationRequest(
+            supporting: [module])
+        {
+            self.downloadProgress = downloader.progress
+            try await downloader.downloadAndInstall()
+        }
+    }
+
+    func deallocate() async {
+        let allocated = await AssetInventory.allocatedLocales
+        for locale in allocated {
+            await AssetInventory.deallocate(locale: locale)
+        }
+    }
+}
+final class BufferConverter {
+    enum Error: Swift.Error {
+        case failedToCreateConverter
+        case failedToCreateConversionBuffer
+        case conversionFailed(NSError?)
+    }
+
+    private var converter: AVAudioConverter?
+
+    func convertBuffer(_ buffer: AVAudioPCMBuffer, to format: AVAudioFormat)
+        throws -> AVAudioPCMBuffer
+    {
+        let inputFormat = buffer.format
+        guard inputFormat != format else {
+            return buffer
+        }
+
+        if converter == nil || converter?.outputFormat != format {
+            converter = AVAudioConverter(from: inputFormat, to: format)
+            converter?.primeMethod = .none
+        }
+
+        guard let converter else {
+            throw Error.failedToCreateConverter
+        }
+
+        let sampleRateRatio =
+            converter.outputFormat.sampleRate / converter.inputFormat.sampleRate
+        let scaledInputFrameLength =
+            Double(buffer.frameLength) * sampleRateRatio
+        let frameCapacity = AVAudioFrameCount(
+            scaledInputFrameLength.rounded(.up)
+        )
+
+        guard
+            let conversionBuffer = AVAudioPCMBuffer(
+                pcmFormat: converter.outputFormat,
+                frameCapacity: frameCapacity
+            )
+        else {
+            throw Error.failedToCreateConversionBuffer
+        }
+
+        var nsError: NSError?
+
+        let status = converter.convert(
+            to: conversionBuffer,
+            error: &nsError,
+            withInputFrom: { _, inputStatusPointer in
+                inputStatusPointer.pointee = .haveData
+                return buffer
+            }
+        )
+
+        guard status != .error else {
+            throw Error.conversionFailed(nsError)
+        }
+
+        return conversionBuffer
+    }
+}
